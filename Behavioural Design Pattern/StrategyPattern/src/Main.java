6a3573324b5b490c677e11354f7d9fd70087d9d2
Prem Mehta <pma@Prems-MacBook-Air.local>
2024-10-04 21:20:20 +0530

    Strategy Pattern

--- Behavioural Design Pattern/StrategyPattern/src/Main.java
+++ Behavioural Design Pattern/StrategyPattern/src/Main.java
@@ -2,13 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        PaymentContext paymentContext = new PaymentContext();
+
         PaymentStrategy paymentStrategy = new CreditCardPayment("1234567890");
-        paymentStrategy.pay(100);
+        paymentContext.setPaymentStrategy(paymentStrategy);
+        paymentContext.pay(100);
 
         PaymentStrategy paypalPayment = new PayPalPayment("user@gmail.com");
-        paypalPayment.pay(500);
+        paymentContext.setPaymentStrategy(paypalPayment);
+        paymentContext.pay(500);
 
         PaymentStrategy stripePayment = new StripePayment("stripe123");
-        stripePayment.pay(1500);
+        paymentContext.setPaymentStrategy(stripePayment);
+        paymentContext.pay(1500);
     }
 }
